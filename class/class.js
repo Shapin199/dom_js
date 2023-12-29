568fc2c2b1db78ebb71dceb7211a427360b6ec37
Shapin199 <mdtanvir054321@gmail.com>
2023-12-29 20:31:59 +0600

    All add

---
+++ class/class.js
@@ -0,0 +1,2 @@
+let class1 = document.getElementsByClassName("Boss1")[2];
+class1.innerHTML="Boss Shapin";
---
+++ qurryseletor/qurryseletor.js
@@ -0,0 +1,16 @@
+let class1 = document.querySelector(".Boss1");
+class1.innerHTML="Boss Shapin";
+
+let class2 = document.querySelectorAll(".Boss1")[2];
+class2.innerHTML="Boss Shapin";
+
+function clickMe()
+{
+    // document.write("Hi Boss");
+    document.getElementById("ii").innerHTML= Date();
+    
+}
+// function ck()
+// {
+//     document.getElementsByTagName("h1").innerHTML= Date();
+// }
